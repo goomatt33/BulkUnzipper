ad758ca5569747eabee10a72410fd572d8803ba6
goomatt33 <goomatt33@gmail.com>
2024-02-23 12:40:58 -0500

    Initial

---
+++ Unzipper/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SevenZipExtractor;
+
+Console.WriteLine("Hello, World!");
+
+string sourceDir = ""; // Directory to find the .7z files
+string targetDir = ""; // Directory to unzip the .7z files to
+
+try
+{
+    var files = Directory.EnumerateFiles(sourceDir, "*.7z", SearchOption.AllDirectories);
+
+    foreach(string file in files)
+    {
+        Console.WriteLine(file);
+
+        using (ArchiveFile archive = new ArchiveFile(file))
+        {
+            archive.Extract(targetDir);
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
